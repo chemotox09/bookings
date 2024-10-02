b6644faef74e8e27df5403e6816196691bca06fb
chemotox09 <gabor.tothpok@gmail.com>
2024-10-02 10:41:52 +0200

    addig reservation

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"time"
+
 	"github.com/alexedwards/scs/v2"
 	"github.com/tsawler/bookings-app/pkg/config"
 	"github.com/tsawler/bookings-app/pkg/handlers"
 	"github.com/tsawler/bookings-app/pkg/render"
-	"log"
-	"net/http"
-	"time"
 )
 
 const portNumber = ":8080"
